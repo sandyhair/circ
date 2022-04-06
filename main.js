219492b15c88fb9c627b7adb90d87ef946209bbb
sandyhair <86634848+sandyhair@users.noreply.github.com>
2022-04-05 17:38:17 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+function preload()
+{
+}
+
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized');
+}
+
+function draw()
+{
+    image(video, 0 ,0, 300, 300);
+    circle(30, 20, 20);
+    circle(270, 20, 20);
+    circle(30, 270, 20);
+    circle(270, 270, 20);
+}
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+    console.log(results);
+    console.log("nose x= " + results[0].pose.nose.x);
+    console.log("nose y= " + results[0].pose.nose.y);
+    }
+}
+
+
+function take_snapshot()
+   {
+       save('bigChungus.png');
+   }
+
+
+
